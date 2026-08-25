64ac0f29b51cbe4a1e449c5cf9d8ca130230ee82
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-08-25 13:01:42 +0330

    feat(i18n): enable Persian Turkish English Arabic locales

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["fa", "en"],
+  locales: ["fa", "tr", "en", "ar"],
   defaultLocale: "fa",
   localePrefix: "always",
 });
