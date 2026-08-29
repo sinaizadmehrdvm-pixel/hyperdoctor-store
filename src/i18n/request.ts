fa95e48e4b68ea22bfb3238494fe95bc84cff1ba
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-08-29 16:18:54 +0300

    Use configured business timezone in next-intl

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,6 @@
 import { getRequestConfig } from "next-intl/server";
 import { hasLocale } from "next-intl";
+import { getBusinessTimeZone } from "@/lib/site-data";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
@@ -7,9 +8,11 @@ export default getRequestConfig(async ({ requestLocale }) => {
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
+  const timeZone = await getBusinessTimeZone();
 
   return {
     locale,
+    timeZone,
     messages: (await import(`../messages/${locale}.json`)).default,
   };
 });
