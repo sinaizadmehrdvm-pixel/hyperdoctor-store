6d4607005ae05826526de72cfa8d077d12df2b72
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-09-04 11:47:14 +0300

    Wire use-profile filters into shop layout

--- src/app/[locale]/shop/layout.tsx
+++ src/app/[locale]/shop/layout.tsx
@@ -2,4 +2,4 @@ import { getLocale } from "next-intl/server";
 import { CatalogFilterBar } from "@/components/site/catalog-filter-bar";
 import { getCatalogFacets,getShopFilterState } from "@/lib/queries";
 
-export default async function ShopLayout({children}:{children:React.ReactNode}){const[locale,facets,state]=await Promise.all([getLocale(),getCatalogFacets(),getShopFilterState()]);return <><CatalogFilterBar locale={locale} brands={facets.brands} terms={facets.terms} brandId={state.brandId} termIds={state.termIds}/>{children}</>}
+export default async function ShopLayout({children}:{children:React.ReactNode}){const[locale,facets,state]=await Promise.all([getLocale(),getCatalogFacets(),getShopFilterState()]);return <><CatalogFilterBar locale={locale} brands={facets.brands} terms={facets.terms} brandId={state.brandId} termIds={state.termIds} use={state.use}/>{children}</>}
