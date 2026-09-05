285d31bc1ca2cb5fb4e9c80aa891aa6e08230bed
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-09-05 08:37:34 +0300

    Use published-product scoped shop facets

--- src/app/[locale]/shop/layout.tsx
+++ src/app/[locale]/shop/layout.tsx
@@ -1,5 +1,5 @@
 import { getLocale } from "next-intl/server";
 import { CatalogFilterBar } from "@/components/site/catalog-filter-bar";
-import { getCatalogFacets } from "@/lib/queries";
+import { getPublishedCatalogFacets } from "@/lib/catalog-facets";
 
-export default async function ShopLayout({children}:{children:React.ReactNode}){const[locale,facets]=await Promise.all([getLocale(),getCatalogFacets()]);return <><CatalogFilterBar locale={locale} brands={facets.brands} terms={facets.terms} attributes={facets.attributes}/>{children}</>}
+export default async function ShopLayout({children}:{children:React.ReactNode}){const[locale,facets]=await Promise.all([getLocale(),getPublishedCatalogFacets()]);return <><CatalogFilterBar locale={locale} brands={facets.brands} terms={facets.terms} attributes={facets.attributes}/>{children}</>}
