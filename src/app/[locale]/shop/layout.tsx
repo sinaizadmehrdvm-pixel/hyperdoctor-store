5453131e4b51631a04de42289d43d0d600c7d132
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-09-05 12:44:41 +0300

    Move catalog facets into contextual shop content

--- src/app/[locale]/shop/layout.tsx
+++ src/app/[locale]/shop/layout.tsx
@@ -1,5 +1,1 @@
-import { getLocale } from "next-intl/server";
-import { CatalogFilterBar } from "@/components/site/catalog-filter-bar";
-import { getPublishedCatalogFacets } from "@/lib/catalog-facets";
-
-export default async function ShopLayout({children}:{children:React.ReactNode}){const[locale,facets]=await Promise.all([getLocale(),getPublishedCatalogFacets()]);return <><CatalogFilterBar locale={locale} brands={facets.brands} terms={facets.terms} attributes={facets.attributes}/>{children}</>}
+export default function ShopLayout({children}:{children:React.ReactNode}){return <>{children}</>}
