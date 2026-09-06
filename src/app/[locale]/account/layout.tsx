666ee62e533dddf8ffe8f8fb0f94355fc56b45ba
sinaizadmehrdvm-pixel <sinaizadmehrdvm@gmail.com>
2026-09-06 22:15:11 +0300

    Version 271: noindex customer account routes

--- src/app/[locale]/account/layout.tsx
+++ src/app/[locale]/account/layout.tsx
@@ -1,3 +1,7 @@
+import { privateRouteMetadata } from "@/lib/private-route-metadata";
+
+export const metadata = privateRouteMetadata;
+
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
   return children;
 }
